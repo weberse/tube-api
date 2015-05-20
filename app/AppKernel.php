6cca3c3be67ad379468d76357d537581f041e285
Den <dfitsak@gmail.com>
2015-05-20 20:11:57 +0200

    sensio generator bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,13 +20,13 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new MediaBundle\MediaBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
